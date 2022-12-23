f2392296b825d87aa4bdc4afd53ab1bfe65e810d
jihye0526 <jihye0526>
2022-12-23 21:10:03 +0900

    5.13 Price Chart part Two

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,13 +1,75 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexCharts from "react-apexcharts";
 
 interface ChartProps {
     coinId: string;
 }
 
+interface IHistorical {
+    time_open: number;
+    time_close: number;
+    open: string;
+    high: string;
+    low: string;
+    close: string;
+    volume: string;
+    market_cap: number;
+}
+
 function Chart({coinId} : ChartProps){
-    const {isLoading, data} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
-    return <h1>Chart</h1>
+    const {isLoading, data} = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+
+    return (
+        <div>
+            {isLoading? 
+                "Loading chart..." : 
+                <ApexCharts 
+                    type="line" 
+                    series={[
+                        {
+                            name: "Price",
+                            data: data?.map(price => Number(price.close)) as number[],
+                        }
+                    ]}
+                    options={{ 
+                        theme: {
+                            mode:"dark"
+                        },
+                        chart: {
+                            height:300, 
+                            width:500,
+                            toolbar: {
+                                show: false
+                            },
+                            background: "transparent"
+                        },
+                        stroke: {
+                            curve: "smooth",
+                            width:4
+                        },
+                        grid: {
+                            show: false
+                        },
+                        xaxis: {
+                            labels: {
+                                show: false
+                            },
+                            axisBorder: {
+                                show: false
+                            },
+                            axisTicks: {
+                                show: false
+                            }
+                        },
+                        yaxis: {
+                            show: false
+                        }
+                    }}
+                />
+            }
+        </div>
+    );
 }
 
 export default Chart;
