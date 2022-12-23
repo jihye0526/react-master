d8f09d824ab195c425ecce74d9e66dc5fd00fed8
jihye0526 <jihye0526>
2022-12-23 18:48:25 +0900

    5.9 React Query part One

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const queryClient = new QueryClient();
 
 root.render(
   // <React.StrictMode> ROUTER 5 버전을 사용하면 StrictMode를 사용했을 때 페이지가 넘어가지 않음
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   // </React.StrictMode>
 );
