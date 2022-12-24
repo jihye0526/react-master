4a758fbf4d0150c404c1d01897cb3754501bb811
jihye0526 <samtafe97@naver.com>
2022-12-24 16:06:48 +0900

    6.5 To Do Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { darkTheme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-const queryClient = new QueryClient();
 
 root.render(
-  // <React.StrictMode> ROUTER 5 버전을 사용하면 StrictMode를 사용했을 때 페이지가 넘어가지 않음
   <RecoilRoot>
-    <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={darkTheme}>
       <App />
-    </QueryClientProvider>
+    </ThemeProvider>
   </RecoilRoot>
-  // </React.StrictMode>
 );
