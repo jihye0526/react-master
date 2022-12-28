76e67fc298f7ee1904b690b9ecc794dc26b9275f
jihye0526 <samtafe97@naver.com>
2022-12-28 21:41:36 +0900

    7.2 Drag and Drop part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { useRecoilState, useRecoilValue } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd"
 
 function App(){
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-
-  const onMinutesChange = (e:React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+e.currentTarget.value); // string인 e.currentTarget.value을 +를 붙임으로써 number로 바꿔줌
-  }
-
-  const onHoursChange = (e:React.FormEvent<HTMLInputElement>) => {
-    setHours(+e.currentTarget.value);
-  }
-
+  const onDragEnd = () => {}
   return (
-    <div>
-      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes"></input>
-      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours"></input>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {() => (
+            <ul>
+              <Draggable draggableId="first" index={0}>
+                {() => <li>One</li>}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {() => <li>Two</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 }
 
