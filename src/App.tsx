00abd2b4b62842da9a710b02e5563acbe517a938
jihye0526 <samtafe97@naver.com>
2022-12-29 14:08:12 +0900

    7.7 Performance(React memo)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautif
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
+import DragabbleCard from "./components/DragabbleCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -27,13 +28,6 @@ const Board = styled.div`
   min-height: 200px;
 `;
 
-const Card = styled.div`
-  border-radius: 5px;
-  margin-bottom: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
-
 function App(){
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = ({ draggableId, destination, source }:DropResult) => {
@@ -41,15 +35,9 @@ function App(){
     setToDos(oldToDos => {
       const toDosCopy = [...oldToDos];
       // 1) Delete item on source.index
-      console.log("Delete item on", source.index);
-      console.log(toDosCopy);
       toDosCopy.splice(source.index, 1);
-      console.log("Delete item");
-      console.log(toDosCopy);
       // 2) Put back the item on the destination.index
-      console.log("Put back", draggableId, "on ", destination.index);
       toDosCopy.splice(destination?.index, 0, draggableId);
-      console.log(toDosCopy);
       return toDosCopy;
     });
   }
@@ -61,15 +49,8 @@ function App(){
             {(provided) => (
               <Board ref={provided.innerRef} {...provided.droppableProps}>
                 {toDos.map((toDo, idx) => 
-                  <Draggable key={toDo} draggableId={toDo} index={idx}>
-                    {(provided) => 
-                      <Card ref={provided.innerRef} 
-                      {...provided.draggableProps} 
-                      {...provided.dragHandleProps}
-                      >
-                        {toDo}
-                    </Card>}
-                  </Draggable>)}
+                    <DragabbleCard key={toDo} toDo={toDo} idx={idx}/>
+                  )}
                   {provided.placeholder}
               </Board>
             )}
