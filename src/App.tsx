c78d0480447199cdae5a694ce74d47939f962e93
jihye0526 <samtafe97@naver.com>
2022-12-29 15:04:50 +0900

    7.10 Cross Board Movement

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ function App(){
   const onDragEnd = (info:DropResult) => {
     console.log(info)
     const {draggableId, source, destination} = info;
+    if (!destination) return;
     if(destination?.droppableId === source.droppableId){
       setToDos(allBoards => {
         const boardCopy = [...allBoards[source.droppableId]];
@@ -34,6 +35,20 @@ function App(){
         return {
           ...allBoards,
         [source.droppableId] : boardCopy};
+      });
+    }
+
+    if(destination?.droppableId !== source.droppableId){
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId] : sourceBoard,
+          [destination.droppableId] : destinationBoard
+        }
       });
     }
   }
