070944c90e602d700cf890c189d73d705faa5fbd
jihye0526 <samtafe97@naver.com>
2022-12-31 17:03:47 +0900

    8.12 Slider part One

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -17,43 +18,52 @@ const Box = styled(motion.div)`
   border-radius: 40px;
   position: absolute;
   top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial: {
-    opacity: 0,
-    scale: 0
-  },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    rotateZ: 360
-  },
-  leaving: {
+const box = {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
-    y: 50
-  }
-}
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+  exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 1 } },
+};
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => { setShowing((prev) => !prev); }
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible(prev => prev === 10 ? 10: prev + 1);
 
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
       <AnimatePresence>
-        { showing? 
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+          i === visible ? 
           <Box 
-            variants={boxVariants} 
-            initial="initial" 
-            animate="visible" 
-            exit="leaving"
-          /> : 
-        null }
+            key={i}
+            variants={box}
+            initial="invisible"
+            animate="visible"
+            exit="exit"
+          >
+              {i}
+          </Box> : 
+          null
+        ))}
       </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
