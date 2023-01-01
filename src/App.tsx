6789b8198b1a6af5b9459686ade62c051428148b
jihye0526 <samtafe97@naver.com>
2023-01-01 14:20:12 +0900

    9.2 Header part Two

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App(){
     <Router>
       <Header />
       <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
         <Route path="/tv">
           <Tv />
         </Route>
         <Route path="/search">
           <Search />
+        </Route>
+        <Route path="/"> {/* 모든 경로는 /로 시작하기 때문에 맨 마지막에 넣어야함. 그렇지 않으면 /에서 모든 경로가 걸림 */}
+          <Home />
         </Route>
       </Switch>
     </Router>
