5ee90b09fab2e841cdeb479cfb89f6ae23f2651e
jihye0526 <samtafe97@naver.com>
2023-01-01 13:32:39 +0900

    9.1 Header part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+
 function App(){
   return (
-    null
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
