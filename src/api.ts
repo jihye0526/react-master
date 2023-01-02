26680072b9d4eb02dabb9b98b2db06d37e2e4989
jihye0526 <samtafe97@naver.com>
2023-01-02 21:32:15 +0900

    9.6 Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,24 @@
 const API_KEY = "03e63f4a6113b7712273d43c6c0662bf";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMovie {
+    id: number;
+    backdrop_path: string;
+    poster_path: string;
+    title: string;
+    overview: string;
+}
+
+export interface IGetMoviesResult {
+    dates: {
+        maximun: string;
+        minimum: string;
+    };
+    page: number;
+    results: IMovie[];
+    total_pages: number;
+    total_results: number
+}
 
 export function getMovies(){
     return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(res => res.json());
