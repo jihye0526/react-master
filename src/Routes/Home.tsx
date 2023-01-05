4f51dc1941d1db69e16a40fc2daa332ec20fd5b9
jihye0526 <samtafe97@naver.com>
2023-01-05 21:46:12 +0900

    9.13 Movie Modal part Three

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -96,6 +96,31 @@ const BigMovie = styled(motion.div)`
     left: 0;
     right: 0;
     margin: 0 auto;
+    border-radius: 15px;
+    overflow: hidden;
+    background-color: ${(props) => props.theme.black.lighter};
+`;
+
+const BigCover = styled.div`
+    width: 100%;
+    background-size: cover;
+    background-position: center center;
+    height: 400px;
+`;
+
+const BigTitle = styled.h3`
+    color: ${(props) => props.theme.white.lighter};
+    padding: 20px;
+    font-size: 46px;
+    position: relative;
+    top: -80px;
+`;
+
+const BigOverview = styled.p`
+    padding: 20px;
+    position: relative;
+    top: -80px;
+    color: ${(props) => props.theme.white.lighter};
 `;
 
 const rowVariants = {
@@ -158,6 +183,7 @@ function Home(){
     const toggleLeaving = () => setLeaving(prev => !prev);
     const onBoxClicked = (movieId:number) => { history.push(`/movies/${movieId}`) }; 
     const onOverlayClick = () => history.push("/");
+    const clickedMovie = bigMovieMatch?.params.movieId && data?.results.find(movie => movie.id+"" === bigMovieMatch.params.movieId)
 
     return (
         <Wrapper>
@@ -207,7 +233,13 @@ function Home(){
                             <BigMovie 
                                 layoutId={bigMovieMatch.params.movieId}
                                 style={{ top: scrollY.get() + 100 }}
-                            >hello</BigMovie>
+                            >{clickedMovie && (
+                                <>
+                                    <BigCover style={{ backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(clickedMovie.backdrop_path, "w500")})` }}/>
+                                    <BigTitle>{clickedMovie.title}</BigTitle>
+                                    <BigOverview>{clickedMovie.overview}</BigOverview>
+                                </>
+                            )}</BigMovie>
                         </> : null}
                     </AnimatePresence>
                 </>
